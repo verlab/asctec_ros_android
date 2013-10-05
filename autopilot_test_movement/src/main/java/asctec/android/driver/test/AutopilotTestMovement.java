fd49f177caea9300c34e8afadd956b01715f6a0c
dav <dav@much-MS-7593.(none)>
2013-10-04 23:24:16 -0300

    initial projects structure

---
+++ autopilot_test_movement/src/main/java/asctec/android/driver/test/AutopilotTestMovement.java
@@ -0,0 +1,15 @@
+package asctec.android.driver.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AutopilotTestMovement extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
